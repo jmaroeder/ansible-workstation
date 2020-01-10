4cdde017b42b2e2938a9324bf7530dc98fee8934
James Roeder <jmaroeder@gmail.com>
2020-01-10 15:40:49 -0500

    Adding more macos customizations

--- setup.sh
+++ setup.sh
@@ -27,5 +27,10 @@ if test ! $(which ansible); then
     fi
 fi
 
+# Prompt for creds if necessary
+sudo -v
+# Keep-alive: update existing `sudo` time stamp until `.macos` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 set -x
-ansible-pull --ask-become-pass --url https://github.com/jmaroeder/ansible-workstation.git
+ansible-pull --url https://github.com/jmaroeder/ansible-workstation.git
