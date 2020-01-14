b7899546b7c8a6cc46ba9221c0d260d4be607ebb
James Roeder <jmaroeder@gmail.com>
2020-01-14 14:33:00 -0500

    Skip vault - doesn't work with ansible-pull

--- install.sh
+++ install.sh
@@ -34,4 +34,4 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 
 set -x
-ansible-pull --url https://github.com/jmaroeder/ansible-workstation.git --ask-vault-pass
+ansible-pull --url https://github.com/jmaroeder/ansible-workstation.git
