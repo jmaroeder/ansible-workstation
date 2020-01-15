1b713e981b88107016b2bb2f2e807c5ebd67c862
James Roeder <jmaroeder@gmail.com>
2020-01-15 13:17:18 -0500

    Remove killalls

--- files/macos.sh
+++ files/macos.sh
@@ -955,33 +955,4 @@ defaults write org.m0k.transmission RandomPort -bool true
 # Bypass the annoyingly slow t.co URL shortener
 # defaults write com.tapbots.TweetbotMac OpenURLsDirectly -bool true
 
-###############################################################################
-# Kill affected applications                                                  #
-###############################################################################
-
-for app in \
-	"Activity Monitor" \
-	"Address Book" \
-	"Calendar" \
-	"cfprefsd" \
-	"Contacts" \
-	"Dock" \
-	"Finder" \
-	"Google Chrome Canary" \
-	"Google Chrome" \
-	"Mail" \
-	"Messages" \
-	"Opera" \
-	"Photos" \
-	"Safari" \
-	"SizeUp" \
-	"Spectacle" \
-	"SystemUIServer" \
-	# "Terminal" \
-	"Transmission" \
-	"Tweetbot" \
-	"Twitter" \
-	"iCal"; do
-	killall "${app}" &> /dev/null || true
-done
 echo "Done. Note that some of these changes require a logout/restart to take effect."
